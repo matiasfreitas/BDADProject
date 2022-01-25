aadb1ad131a14badee5bfe94097d967672027d8c
Matias Vaz <matiasfreitasguimaraes@gmail.com>
2022-01-25 00:50:33 +0000

    Criar almost corrected

--- criar.sql
+++ criar.sql
@@ -10,125 +10,86 @@ DROP TABLE IF EXISTS Object;
 CREATE TABLE Object
 (
     OBJID       INTEGER PRIMARY KEY,
-    name        VARCHAR(25)                         NOT NULL,
-    maxLife     INTEGER                             NOT NULL CHECK (MaxLife >= 0),
-    currentLife INTEGER                             NOT NULL CHECK (CurrentLife >= 0 and CurrentLife <= MaxLife),
-    TPID        VARCHAR(30) REFERENCES Type (name)      NOT NULL
+    name        VARCHAR(25) NOT NULL,
+    maxLife     INTEGER     NOT NULL CHECK (MaxLife >= 0),
+    currentLife INTEGER     NOT NULL CHECK (CurrentLife >= 0 and CurrentLife <= MaxLife),
+    x           INTEGER     NOT NULL CHECK (x >= 0),
+    y           INTEGER     NOT NULL CHECK (y >= 0),
+    TPName      VARCHAR(30) REFERENCES Type (name) ON DELETE SET NULL NOT NULL
 );
 
 -- Table: Item
 DROP TABLE IF EXISTS Item;
 CREATE TABLE Item
 (
-    OBJID       INTEGER PRIMARY KEY,
-    name        VARCHAR(25)                         NOT NULL,
-    maxLife     INTEGER                             NOT NULL CHECK (MaxLife >= 0),
-    currentLife INTEGER                             NOT NULL CHECK (CurrentLife >= 0 and CurrentLife <= MaxLife),
-    weight      INTEGER                             NOT NULL CHECK (Weight >= 0 and Weight <= 2048),
+    ITEMID      INTEGER PRIMARY KEY,
+    weight      INTEGER                                                 NOT NULL CHECK (Weight >= 0 and Weight <= 2048),
     description VARCHAR(150),
-    TPID        VARCHAR(30) REFERENCES Type (name)      NOT NULL
+    owner       VARCHAR(30) REFERENCES Object (OBJID),
+    OBJID       VARCHAR(30) REFERENCES Object (OBJID) ON DELETE CASCADE NOT NULL
 );
 
 -- Table: NonPlayChar
 DROP TABLE IF EXISTS NonPlayChar;
 CREATE TABLE NonPlayChar
 (
-    OBJID             INTEGER PRIMARY KEY,
-    name              VARCHAR(30)                         NOT NULL,
-    maxLife           INTEGER                             NOT NULL CHECK (MaxLife >= 0),
-    currentLife       INTEGER                             NOT NULL CHECK (CurrentLife >= 0 and CurrentLife <= MaxLife),
-    speed             INTEGER                             NOT NULL CHECK (Speed >= 0),
-    baseDamage        INTEGER                             NOT NULL CHECK (BaseDamage >= 0),
-    magicalBaseDamage INTEGER                             NOT NULL CHECK (MagicalBaseDamage >= 0),
-    level             INTEGER                             NOT NULL CHECK (Level >= 0 and Level <= 20),
-    hitBox            INTEGER                             NOT NULL CHECK (HitBox >= 0),
-    killable          INTEGER                             NOT NULL CHECK (Killable = 0 or Killable = 1),
-    TPID              VARCHAR(30) REFERENCES Type (name)      NOT NULL
-    CHECK (BaseDamage != 0 or MagicalBaseDamage != 0)
+    NPLAYID           INTEGER PRIMARY KEY,
+    speed             INTEGER                                                 NOT NULL CHECK (Speed >= 0),
+    baseDamage        INTEGER                                                 NOT NULL CHECK (BaseDamage >= 0),
+    magicalBaseDamage INTEGER                                                 NOT NULL CHECK (MagicalBaseDamage >= 0),
+    level             INTEGER                                                 NOT NULL CHECK (Level >= 0 and Level <= 20),
+    hitBox            INTEGER                                                 NOT NULL CHECK (HitBox >= 0),
+    killable          INTEGER                                                 NOT NULL CHECK (Killable = 0 or Killable = 1),
+    OBJID             VARCHAR(30) REFERENCES Object (OBJID) ON DELETE CASCADE NOT NULL,
+    CONSTRAINT damageCheck CHECK (baseDamage != 0 or magicalBaseDamage != 0)
 );
 
 -- Table: PlayChar
 DROP TABLE IF EXISTS PlayChar;
 CREATE TABLE PlayChar
 (
-    OBJID             VARCHAR(30) PRIMARY KEY,
-    name              VARCHAR(30)                         NOT NULL,
-    maxLife           INTEGER                             NOT NULL CHECK (MaxLife >= 0),
-    currentLife       INTEGER                             NOT NULL CHECK (CurrentLife >= 0 and CurrentLife <= MaxLife),
-    speed             INTEGER                             NOT NULL CHECK (Speed >= 0),
-    baseDamage        INTEGER                             NOT NULL CHECK (BaseDamage >= 0),
-    magicalBaseDamage INTEGER                             NOT NULL CHECK (MagicalBaseDamage >= 0),
-    level             INTEGER                             NOT NULL CHECK (Level >= 0 and Level <= 20),
-    hitBox            INTEGER                             NOT NULL CHECK (HitBox >= 0),
-    strength          INTEGER                             NOT NULL CHECK (Strength >= 0 and Strength <= 20),
-    intelligence      INTEGER                             NOT NULL CHECK (Intelligence >= 0 and Intelligence <= 20),
-    dextery           INTEGER                             NOT NULL CHECK (Dextery >= 0 and Dextery <= 20),
-    maxEnergy         INTEGER                             NOT NULL CHECK (MaxEnergy >= 0),
-    currentEnergy     INTEGER                             NOT NULL CHECK (CurrentEnergy >= 0 and CurrentEnergy <= MaxEnergy),
-    carryWeight       INTEGER                             NOT NULL CHECK (CarryWeight >= 0),
-    TPID              VARCHAR(30) REFERENCES Type (name)  NOT NULL,
-    CHECK (BaseDamage != 0 or MagicalBaseDamage != 0)
+    PLAYID            VARCHAR(30) PRIMARY KEY,
+    speed             INTEGER                                                 NOT NULL CHECK (Speed >= 0),
+    baseDamage        INTEGER                                                 NOT NULL CHECK (BaseDamage >= 0),
+    magicalBaseDamage INTEGER                                                 NOT NULL CHECK (MagicalBaseDamage >= 0),
+    level             INTEGER                                                 NOT NULL CHECK (Level >= 0 and Level <= 20),
+    hitBox            INTEGER                                                 NOT NULL CHECK (HitBox >= 0),
+    strength          INTEGER                                                 NOT NULL CHECK (Strength >= 0 and Strength <= 20),
+    intelligence      INTEGER                                                 NOT NULL CHECK (Intelligence >= 0 and Intelligence <= 20),
+    dextery           INTEGER                                                 NOT NULL CHECK (Dextery >= 0 and Dextery <= 20),
+    maxEnergy         INTEGER                                                 NOT NULL CHECK (MaxEnergy >= 0),
+    currentEnergy     INTEGER                                                 NOT NULL CHECK (CurrentEnergy >= 0 and CurrentEnergy <= MaxEnergy),
+    carryWeight       INTEGER                                                 NOT NULL CHECK (CarryWeight >= 0),
+    OBJID             VARCHAR(30) REFERENCES Object (OBJID) ON DELETE CASCADE NOT NULL,
+    CONSTRAINT damageCheck CHECK (baseDamage != 0 or magicalBaseDamage != 0)
 );
 
-
--- Table: Position
-DROP TABLE IF EXISTS Position;
-CREATE TABLE Position
+-- Table: Action
+DROP TABLE IF EXISTS Action;
+CREATE TABLE Action
 (
-    POSID INTEGER PRIMARY KEY,
-    x     INTEGER,
-    y     INTEGER
+    ACTID            INTEGER PRIMARY KEY,
+    name             VARCHAR(30)                                            NOT NULL,
+    executionTime    INTEGER                                                NOT NULL,
+    currentUse       INTEGER                                                NOT NULL,
+    maxUse           INTEGER                                                NOT NULL,
+    activeTimeWindow INTEGER                                                NOT NULL,
+    hitbox           INTEGER,
+    TPName           VARCHAR(30)                                            REFERENCES Type (name) ON DELETE SET NULL NOT NULL,
+    EFID             VARCHAR(30) REFERENCES Effect (EFID) ON DELETE CASCADE NOT NULL,
+    CONSTRAINT validadteUse CHECK ((currentUse <= maxUse and currentUse >= 0) or (maxUse == 0 and currentUse == NULL))
 );
-
-
 
 -- Table: Effect
 DROP TABLE IF EXISTS Effect;
 CREATE TABLE Effect
 (
     EFID     INTEGER PRIMARY KEY,
-    name     VARCHAR(30)                    NOT NULL UNIQUE,
+    name     VARCHAR(30) NOT NULL UNIQUE,
     duration INTEGER,
-    damage   INTEGER,
-    TPID     INTEGER REFERENCES Type (TPID) NOT NULL
+    damage   INTEGER
 );
 
--- Table: Sprite
-DROP TABLE IF EXISTS SpritePlayChar;
-CREATE TABLE SpritePlayChar
-(
-    SPRID INTEGER PRIMARY KEY,
-    image                                     NOT NULL UNIQUE,
-    OBJID INTEGER REFERENCES PlayChar (OBJID) NOT NULL,
-    ACTID INTEGER REFERENCES ActionP (ACTID)  NOT NULL
-);
-
-DROP TABLE IF EXISTS SpriteNonPlayChar;
-CREATE TABLE SpriteNonPlayChar
-(
-    SPRID INTEGER PRIMARY KEY,
-    image                                        NOT NULL UNIQUE,
-    OBJID INTEGER REFERENCES NonPlayChar (OBJID) NOT NULL,
-    ACTID INTEGER REFERENCES ActionP (ACTID)     NOT NULL
-);
-
-DROP TABLE IF EXISTS SpriteItem;
-CREATE TABLE SpriteItem
-(
-    SPRID INTEGER PRIMARY KEY,
-    image                                    NOT NULL UNIQUE,
-    OBJID INTEGER REFERENCES Item (OBJID)    NOT NULL,
-    ACTID INTEGER REFERENCES ActionP (ACTID) NOT NULL
-);
-
-DROP TABLE IF EXISTS Sprite;
-CREATE TABLE Sprite
-(
-    SPRID INTEGER PRIMARY KEY,
-    Image VARCHAR(30)                        NOT NULL UNIQUE,
-    OBJID INTEGER REFERENCES Object (OBJID)  NOT NULL,
-    ACTID INTEGER REFERENCES ActionP (ACTID) NOT NULL
-);
 
 -- Table: Type
 DROP TABLE IF EXISTS Type;
@@ -141,13 +102,11 @@ CREATE TABLE Type
 DROP TABLE IF EXISTS Scenario;
 CREATE TABLE Scenario
 (
-    SCNID  INTEGER PRIMARY KEY,
-    Name   VARCHAR(30)                      NOT NULL UNIQUE,
-    Width  INTEGER                          NOT NULL CHECK (Width > 0),
-    Height INTEGER                          NOT NULL CHECK (Height > 0),
-    Background                              NOT NULL,
-    TPID   INTEGER REFERENCES Type (TPID)   NOT NULL,
-    STGID  INTEGER REFERENCES Stage (STGID) NOT NULL
+    name    VARCHAR(30) PRIMARY KEY                           NOT NULL UNIQUE,
+    width   INTEGER                                           NOT NULL CHECK (width > 0),
+    height  INTEGER                                           NOT NULL CHECK (height > 0),
+    background                                                NOT NULL,
+    STGName INTEGER REFERENCES Stage (name) ON DELETE CASCADE NOT NULL
 
 );
 
@@ -155,34 +114,49 @@ CREATE TABLE Scenario
 DROP TABLE IF EXISTS Stage;
 CREATE TABLE Stage
 (
-    STGID  INTEGER PRIMARY KEY,
-    Name   VARCHAR(50) NOT NULL UNIQUE,
-    Symbol VarChar     NOT NULL UNIQUE
+    name   VARCHAR(50) PRIMARY KEY NOT NULL UNIQUE,
+    symbol VarChar                 NOT NULL UNIQUE
 );
 
 
--- Table: Action
-DROP TABLE IF EXISTS ActionP;
-CREATE TABLE ActionP
+-- Table: ObjectDo
+DROP TABLE IF EXISTS ObjectDo;
+CREATE TABLE ObjectDo
 (
-    ACTID            INTEGER PRIMARY KEY,
-    Name             VARCHAR(30) NOT NULL,
-    ExecutionTime    INTEGER     NOT NULL,
-    CurrentUse       INTEGER     NOT NULL,
-    MaxUse           INTEGER     NOT NULL,
-    ActiveTimeWindow INTEGER     NOT NULL,
-    Hitbox           INTEGER,
-    CHECK ((CurrentUse <= MaxUse and CurrentUse >= 0) or (MaxUse == 0 and CurrentUse == NULL))
+    image VARCHAR(30)                                          NOT NULL UNIQUE,
+    OBJID INTEGER REFERENCES Object (OBJID) ON DELETE CASCADE  ON UPDATE CASCADE NOT NULL,
+    ACTID INTEGER REFERENCES ActionP (ACTID) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
+    PRIMARY KEY (OBJID, ACTID)
 );
 
--- Table: MultipleModifier
-DROP TABLE IF EXISTS MultipleModifier;
-CREATE TABLE MultipleModifier
+
+-- Table: InteractWith
+DROP TABLE IF EXISTS InteractWith;
+CREATE TABLE InteractWith
 (
-    MMDID    INTEGER PRIMARY KEY,
-    TPID2    INTEGER REFERENCES Type (TPID) NOT NULL,
-    TPID1    INTEGER REFERENCES Type (TPID) NOT NULL,
-    Multiple FLOAT                          NOT NULL CHECK ( Multiple >= 0 and Multiple <= 4 )
+    fromTPName INTEGER REFERENCES Type (name) ON DELETE CASCADE  ON UPDATE CASCADE NOT NULL,
+    toTPName   INTEGER REFERENCES Type (name) ON DELETE CASCADE  ON UPDATE CASCADE NOT NULL,
+    Multiple   FLOAT                          NOT NULL CHECK ( Multiple >= 0 and Multiple <= 4 ),
+    PRIMARY KEY (fromTPName, toTPName)
+);
+
+
+-- Table: EffectsAffectsObjects
+DROP TABLE IF EXISTS EffectsAffectsObjects;
+CREATE TABLE EffectsAffectsObjects
+(
+    EFID  INTEGER REFERENCES Effect (EFID) ON DELETE CASCADE  ON UPDATE CASCADE NOT NULL,
+    OBJID INTEGER REFERENCES Object (OBJID) ON DELETE CASCADE  ON UPDATE CASCADE NOT NULL,
+    PRIMARY KEY (EFID, OBJID)
+);
+
+
+DROP TABLE IF EXISTS Drops;
+CREATE TABLE Drops
+(
+    ITEMID  INTEGER REFERENCES Item (ITEMID)         NOT NULL,
+    OBJID INTEGER REFERENCES NonPlayChar (NPLAYID) NOT NULL,
+    PRIMARY KEY (ITEMID, OBJID)
 );
 
 
