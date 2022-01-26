2d11dc1dc8951694d4303dcf5cb6a391cf2f1577
Matias Vaz <matiasfreitasguimaraes@gmail.com>
2022-01-26 19:50:40 +0000

    Querys corrections

--- int10.sql
+++ int10.sql
@@ -3,4 +3,14 @@
 .nullvalue NULL
 
 
-SELECT
+SELECT name, baseDamage*InteractWith.multiple, magicalBaseDamage*InteractWith.multiple, level, maxLife, TPName
+FROM (Object JOIN NonPlayChar Nplay on Nplay.NPLAYID == Object.OBJID),InteractWith
+where InteractWith.toTPName = 'water' and Object.TPName = InteractWith.fromTPName
+UNION
+SELECT name, magicalBaseDamage*InteractWith.multiple, baseDamage*InteractWith.multiple, level, maxLife, TPName
+FROM (Object JOIN PlayChar play on play.PLAYID == Object.OBJID), InteractWith
+where InteractWith.toTPName = 'water' and Object.TPName = InteractWith.fromTPName
+ORDER BY magicalBaseDamage*InteractWith.multiple desc
+
+
+
